3edcd1a8916c0e5941af00818531aa7d770777ba
Waffless-ua <wafflesswithoutwaffles@gmail.com>
2026-02-07 18:30:46 +0200

    Added Checkmate and Stalemate

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -10,13 +10,13 @@ namespace ChessLogic
     {
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
-
+        public Result Result { get; private set; } = null;
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
             Board = board;
         }
-
+        public bool IsGameOver() => Result != null;
 
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
         {
@@ -28,10 +28,40 @@ namespace ChessLogic
             return moveCandidates.Where(move => move.IsLegal(Board));
         }
 
+
         public void MakeMove(Move move)
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+
+
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+
+            return moveCandidates.Where(move => move.IsLegal(Board));
+        }
+
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+
         }
 
     }
